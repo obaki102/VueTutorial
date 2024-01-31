bf5ede0b07697a611e01c2d9014cb205a9e86dcf
joshuajpiluden@gmail.com <joshuajpiluden@Gmail.com>
2024-01-31 21:18:37 +0800

    Methods, computed and watch

--- vue-coposition-api-basic/src/App.vue
+++ vue-coposition-api-basic/src/App.vue
@@ -1,7 +1,3 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
   <nav>
     <RouterLink to="/">Home</RouterLink>
@@ -11,7 +7,17 @@ import { RouterLink, RouterView } from 'vue-router'
   <RouterView />
 </template>
 
-<style scoped>
+<style>
+@import '@/assets/base.css';
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+
+  font-weight: normal;
+}
+
 header {
   line-height: 1.5;
   max-height: 100vh;
@@ -22,55 +28,18 @@ header {
   margin: 0 auto 2rem;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
   }
 }
+
+
 </style>
--- vue-coposition-api-basic/src/views/HomeView.vue
+++ vue-coposition-api-basic/src/views/HomeView.vue
@@ -1,7 +1,59 @@
-
-
 <template>
   <div class="home">
-    <h1>Home</h1>
+    <h2>{{ appTitle }}</h2>
+    <h3>{{ counterData.title }}:</h3>
+    <div>
+      <button @click="decreaseCounter(2)" class="btn">-2</button>
+      <button @click="decreaseCounter(1)" class="btn">-</button>
+      <span class="counter">{{ counterData.count }}</span>
+      <button @click="increaseCounter(1)" class="btn">+</button>
+      <button @click="increaseCounter(2)" class="btn">+2</button>
+    </div>
+    <p>TThis counterb is {{ oddOrEven }}</p>
+    <div class="edit">
+      <h4>Edit counter title</h4>
+      <input type="text" v-model="counterData.title">
+    </div>
   </div>
 </template>
+
+<script setup>
+import { computed, reactive, watch } from 'vue'
+
+const appTitle = 'My App'
+const counterData = reactive({
+  count: 0,
+  title: 'My Counter'
+})
+
+watch(() => counterData.count, (newCount) => {
+  if (newCount == 20)
+    alert('20 reach')
+})
+
+const oddOrEven = computed(() => counterData.count % 2 === 0 ? 'even' : 'odd');
+const increaseCounter = value => {
+  counterData.count += value
+}
+
+const decreaseCounter = value => {
+  counterData.count -= value
+}
+</script>
+
+<style>
+.home {
+  text-align: center;
+  padding: 20px;
+}
+
+.btn,
+.counter {
+  font-size: 40px;
+  margin: 10px
+}
+
+.edt {
+  margin-top: 60px;
+}
+</style>
